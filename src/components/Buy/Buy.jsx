f1356383bf57be2e2ff3dedf54a99f017fc9a55a
yihnatchuk <yihnatchuk@griddynamics.com>
2023-02-09 06:59:23 -0800

    navigate

--- src/components/Buy/Buy.jsx
+++ src/components/Buy/Buy.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Footer } from '../Footer/Footer'
 import { Header } from '../Header/Header'
+import { useNavigate } from "react-router-dom";
 import poster from '../../assets/poster.jpeg';
 import styles from './Buy.module.scss';
 
 export const Buy = () => {
+  const navigate = useNavigate();
   return (
     <>
         <Header />
         <section className={styles.buy}>
             <h1>Let's buy MacBook</h1>
             <figure>
-                <img src={poster} alt="poster" />
+                <img src={poster} alt="poster" onClick={() => navigate('/products')} />
             </figure>
         </section>
         <Footer />
